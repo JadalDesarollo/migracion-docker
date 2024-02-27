647d3e915dfde3ca1876655b4d80502f98a47be9
renzo <desarrollo.jadalsoftware@gmail.com>
2024-02-27 13:49:00 -0500

    fix:report_day

--- resources/views/reportexcel/report_day_sale.blade.php
+++ resources/views/reportexcel/report_day_sale.blade.php
@@ -2,34 +2,23 @@
 <table class="table table-bordered" style="border: 1px solid black;">
     <thead>
         <tr>
-            <td colspan="4" style="text-align: center; background-color: #071846; color: #FFFFFF;"><strong>{{ $data['title'] }}</strong></td>
+            <td colspan="6" style="text-align: center; background-color: #071846; color: #FFFFFF;"><strong>{{ $data['title'] }}</strong></td>
             <td colspan="3" style="text-align: center; background-color: #071846; color: #FFFFFF;"><strong>Fecha: {{ $data['date'] }}</strong></td>
             <td colspan="3" style="text-align: center; background-color: #071846; color: #FFFFFF;"><strong>Hora: {{ date('H:i:s') }}</strong></td>
         </tr>
         <tr>
             <th colspan="3" style="text-align: center; background-color: #071846; color: #FFFFFF;"><strong>Estacion: {{ $data['establishment'] }}</strong></th>
-            <th colspan="5" style="text-align: center; background-color: #071846; color: #FFFFFF;"><strong>Del {{ date('d/m/Y', strtotime($data['desde'])) }} Al {{ date('d/m/Y', strtotime($data['hasta'])) }}</strong></th>
+            <th colspan="7" style="text-align: center; background-color: #071846; color: #FFFFFF;"><strong>Del {{ date('d/m/Y', strtotime($data['desde'])) }} Al {{ date('d/m/Y', strtotime($data['hasta'])) }}</strong></th>
             <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;"><strong>Usuario: {{ $data['user'] }}</strong></th>
         </tr>
         <tr>
             <!-- Aquí ajusta los encabezados según tus datos -->
             <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">Fecha</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">84 OCT(produc)-Galones</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">84 OCT(produc)-Soles</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">90 OCT-Galones(produc)</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">90 OCT-Galones(product)</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">95 OCT-Galones(produc)</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">95 OCT-Galones(product)</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">97 OCT-Galones(produc)</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">97 OCT-Galones(product)</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">REGULAR-Galones(produc)</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">REGULAR-Galones(product)</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">PREMIUM(produc)</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">PREMIUM(product)</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">GAS GLP(Galones)</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">GAS GLP(Soles)</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">Total-Galones(delaFila)</th>
-            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">Total-Soles(delaFila)</th>
+            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">Local</th>
+            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">Fecha Venta</th>
+            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">Cantidad</th>
+            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">Monto</th>
+            <th colspan="2" style="text-align: center; background-color: #071846; color: #FFFFFF;">Nombre</th>
         </tr>
     </thead>
     <tbody>
@@ -37,13 +26,14 @@
         @foreach ($data['content'] as $row)
         <tr>
             <!-- Ajusta aquí para mostrar los datos correctos -->
-            <td>{{ $row->sale_date_v }}</td>
-            <td>{{ $row->id_local_v }}</td>
-            <td>{{ $row->sale_date_v }}</td>
-            <td>{{ $row->quantity_v }}</td>
-            <td>{{ $row->total_amount_v }}</td>
-            <td>{{ $row->product_name_v }}</td>
+            <td colspan="2" >{{ $row->sale_date_v }}</td>
+            <td colspan="2" >{{ $row->id_local_v }}</td>
+            <td colspan="2" >{{ $row->sale_date_v }}</td>
+            <td colspan="2" >{{ $row->quantity_v }}</td>
+            <td colspan="2" >{{ $row->total_amount_v }}</td>
+            <td colspan="2" >{{ $row->product_name_v }}</td>
         </tr>
         @endforeach
     </tbody>
 </table>
+
